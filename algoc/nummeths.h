c08e281c7f69e7a7c2c223ef85e3c4ccc82b242f
Rino Jose <rjose@ejorp.com>
2013-06-18 22:47:24 -0700

    Interpolate compiles

---
+++ algoc/nummeths.h
@@ -0,0 +1,7 @@
+#ifndef NUMMETHS_H
+#define NUMMETHS_H
+
+int interpol(const double *x, const double *fx, int n,
+                                                  double *z, double *pz, int m);
+
+#endif
