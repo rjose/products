053c2be7e8d634538910e61297e7652503e19d91
Rino Jose <rjose@ejorp.com>
2013-06-18 23:56:25 -0700

    Least squares compiles

--- algoc/nummeths.h
+++ algoc/nummeths.h
@@ -4,4 +4,6 @@
 int interpol(const double *x, const double *fx, int n,
                                                   double *z, double *pz, int m);
 
+void lsqe(const double *x, const double *y, int n, double *b1, double *b0);
+
 #endif
