154690e3f7eadfb28b8ba4c30d07d06c2dc810fe
Rino Jose <rjose@linkedin.com>
2013-06-20 16:21:30 -0700

    Writer writes out triage column

--- qplan/v2/tests/tests.lua
+++ qplan/v2/tests/tests.lua
@@ -6,8 +6,8 @@ local LuaUnit = require('luaunit')
 -- require('test_select')
 -- require('test_work')
 -- require('test_plan')
-require('test_read')
--- require('test_write')
+-- require('test_read')
+require('test_write')
 
 -- Shell functions
 -- require('test_triage')
