cc3c53e8b7e988ea2606de2f303987f436f3c901
Rino Jose <rjose@ejorp.com>
2013-07-01 16:33:44 -0700

    Added unit tests from router prototype

--- qplan/v4/modules/tests/tests.lua
+++ qplan/v4/modules/tests/tests.lua
@@ -3,6 +3,6 @@ package.path = package.path .. ";../?.lua"
 local LuaUnit = require('luaunit')
 
 require('test_parse_request')
---require('test_request_router')
+require('test_route_request')
 
 LuaUnit:run()
