daaeb582b83a9a90b0a2e8de4ea9935901635da3
Rino Jose <rjose@linkedin.com>
2013-07-11 11:33:00 -0700

    Set up test5

---
+++ websockets/tests/test5.m
@@ -0,0 +1,39 @@
+#include <err.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "../ws.h"
+
+#import "Testing.h"
+
+/* ============================================================================
+ * Test data
+ */
+
+// TODO: Add filenames
+
+/* ============================================================================
+ * Expected results
+ */
+
+
+// TODO: Add filenames
+
+/* ============================================================================
+ * Main
+ */
+
+int main()
+{
+        const char *message_body = NULL;
+        uint8_t *frame = NULL;
+
+        /*
+         * Read small message
+         */
+        START_SET("Extract medium message");
+        END_SET("Extract medium message");
+
+        return 0;
+}
