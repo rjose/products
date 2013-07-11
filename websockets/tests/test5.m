9f1bf6716df07146492572e86a59b13e22b6f37d
Rino Jose <rjose@linkedin.com>
2013-07-11 14:04:16 -0700

    Can construct medium message

--- websockets/tests/test5.m
+++ websockets/tests/test5.m
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
 
 #include "../ws.h"
 
 #import "Testing.h"
 
 /* ============================================================================
+ * Static declarations
+ */
+static void load_data(uint8_t *, size_t, const char *);
+
+/* ============================================================================
  * Test data
  */
 
-// TODO: Add filenames
-
-/* ============================================================================
- * Expected results
- */
+static const char med126txt[] = "./data/med-126.txt";
+static char med126[126 + 1];
 
 
 // TODO: Add filenames
@@ -24,15 +28,49 @@
  * Main
  */
 
+/*
+ * NOTE: Assuming dst has enough capacity for len + 1
+ */
+static void load_data(uint8_t *dst, size_t len, const char *filename)
+{
+        FILE *file;
+
+        if ((file = fopen(filename, "r")) == NULL) {
+                printf(strerror(errno));
+                exit(errno);
+        }
+
+        if (fread((void *)dst, sizeof(char), len, file) != len)
+                exit(-1);
+
+        if (fclose(file) != 0) {
+                printf(strerror(errno));
+                exit(errno);
+        }
+
+        dst[len] = '\0';
+
+        return;
+}
+
 int main()
 {
         const char *message_body = NULL;
-        uint8_t *frame = NULL;
+        const uint8_t *frame = NULL;
 
         /*
-         * Read small message
+         * Construct frame for medium message
          */
         START_SET("Extract medium message");
+        load_data(med126, 126, med126txt);
+        frame = ws_make_text_frame(med126, NULL);
+        pass(0x81 == frame[0], "Check first byte of med126 frame");
+        pass(0x7e == frame[1], "Check second byte of med126 frame");
+        pass(0x00 == frame[2], "Check 1st len byte of med126 frame");
+        pass(0x7e == frame[3], "Check 2nd len byte of med126 frame");
+        pass(0x54 == frame[4], "Check 1st msg byte 'T' of med126 frame");
+        pass(0xa == frame[129], "Check last msg byte '\\n' of med126 frame");
+
         END_SET("Extract medium message");
 
         return 0;
--- websockets/ws.c
+++ websockets/ws.c
@@ -14,6 +14,10 @@
 #define BUF_LENGTH 200
 
 #define SHORT_MESSAGE_LEN 125
+#define MED_MESSAGE_LEN 0xFFFF 
+#define MED_MESSAGE_KEY 126
+#define LONG_MESSAGE_KEY 127
+
 
 #define MASK_OFFSET 2
 
@@ -150,45 +154,78 @@ static uint8_t toggle_mask(uint8_t c, size_t index, const uint8_t mask[4])
 }
 
 
+/*
+ * NOTE: This function will always set the FIN bit to 1. If you want to send
+ * fragments, set this to 0 once you get the frame back.
+ */
 const uint8_t *ws_make_text_frame(const char *message, const uint8_t mask[4])
 {
-        int i;
+        uint64_t i;
         size_t message_len;
-        size_t mask_len = 0;
+        size_t mask_len;
+        size_t num_len_bytes; /* Number of extended payload len bytes */
         uint8_t byte0, byte1;     /* First two bytes of the frame */
+        uint64_t tmp;
+        size_t frame_len;
         uint8_t *result = NULL;
 
         /* We know this is a text frame */
         byte0 = WS_FRAME_OP_TEXT;
+        byte0 |= WS_FRAME_FIN;
 
         /* If a mask is specified, set the mask bit */
-        byte1 = 0;
-        if (mask) {
-                byte1 = WS_FRAME_MASK;
-                mask_len = 4;
-        }
+        byte1 = mask ? WS_FRAME_MASK : 0;
 
+        /*
+         * Figure out the length of the frame and then allocate memory. This
+         * involves figuring out if we need a mask, if we need extra length
+         * bytes, and how big the message is.
+         */
+        mask_len = mask ? 4 : 0;
         message_len = strlen(message);
         if (message_len <= SHORT_MESSAGE_LEN) {
-                byte0 |= WS_FRAME_FIN;
+                num_len_bytes = 0;
                 byte1 |= message_len;
-
-                if ((result = malloc(2 + mask_len + message_len)) == NULL)
-                        err_abort(-1, "Can't allocate memory for ws_make_text_frame");
-
-                result[0] = byte0;
-                result[1] = byte1;
-
-                /* Add mask */
-                if (mask)
-                        for (i = 0; i < mask_len; i++)
-                                result[2+i] = mask[i];
-
-                for (i = 0; i < message_len; i++) {
-                        result[2 + mask_len + i] = toggle_mask(message[i], i, mask);
-                }
         }
-        // TODO: Handle other message sizes
+        else if (message_len > SHORT_MESSAGE_LEN &&
+                                               message_len <= MED_MESSAGE_LEN) {
+                num_len_bytes = 2;
+                byte1 |= MED_MESSAGE_KEY;
+        }
+        else {
+                num_len_bytes = 8;
+                byte1 |= LONG_MESSAGE_KEY;
+        }
+        frame_len = 2 + num_len_bytes + mask_len + message_len;
+        if ((result = (uint8_t *)malloc(frame_len)) == NULL)
+                err_abort(-1, "Can't allocate memory for ws_make_text_frame");
+
+        /*
+         * Write data into the frame. First, we'll write the first 2 bytes
+         * that we've constructed. After this comes the extended payload
+         * length (if needed). After that is the mask (if needed). Finally, we
+         * write our message.
+         */
+        result[0] = byte0;
+        result[1] = byte1;
+
+        /* Write extended length */
+        tmp = message_len;
+        for (i = num_len_bytes; i > 0; i--) {
+                result[2 + i - 1] = tmp & 0xFF;
+                tmp = message_len >> 8;
+        }
+        
+        /* Write mask */
+        if (mask)
+                for (i = 0; i < mask_len; i++)
+                        result[2 + num_len_bytes + i] = mask[i];
+
+        /* Write message */
+        for (i = 0; i < message_len; i++) {
+                result[2 + num_len_bytes + mask_len + i] =
+                                       toggle_mask(message[i], i, mask);
+        }
 
         return result;
 }
