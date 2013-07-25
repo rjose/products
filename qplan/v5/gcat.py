680e0df91aa2a6d752036e0da44af4dc26121206
= <rjose@ejorp.com>
2013-07-25 08:28:50 -0700

    1: Write google doc spreadsheet to stdout

---
+++ qplan/v5/gcat.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import os
+import sys
+import gspread
+
+# Read config info
+config = ConfigParser.ConfigParser()
+config.readfp(open(os.path.expanduser('~/.gcat.conf')))
+
+# Log in
+user = config.get('User info', 'user')
+password = config.get('User info', 'password')
+gc = gspread.login(user, password)
+
+# Read in spreadsheet source info
+source_info = ConfigParser.ConfigParser()
+source_info.readfp(sys.stdin)
+sections = source_info.sections()
+
+def cat_tables(section):
+        print "=====%s" % section
+        for p in source_info.items(section):
+                [spreadsheet_key, worksheet_index] = p[1].split(":")
+                spreadsheet = gc.open_by_key(spreadsheet_key)
+                worksheet = spreadsheet.get_worksheet(int(worksheet_index))
+                list_of_lists = worksheet.get_all_values()
+                for row in list_of_lists:
+                        print '\t'.join(row)
+        return
+
+for s in sections:
+        cat_tables(s)
